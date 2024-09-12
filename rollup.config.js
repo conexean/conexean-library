ca480ad9f03ff1847e831f1da365c277152639e7
Pedro Correa <pedro.nec@hotmail.com>
2024-09-12 02:40:48 -0300

    atualizações

--- rollup.config.js
+++ rollup.config.js
@@ -1,58 +1,45 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
-import { terser } from 'rollup-plugin-terser';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import postcss from 'rollup-plugin-postcss';
-import dts from 'rollup-plugin-dts';
+import { terser } from 'rollup-plugin-terser';
 
-const packageJson = require('./package.json');
-
-const config = [
-  {
-    input: 'src/index.ts',
-    output: [
-      {
-        file: packageJson.main,
-        format: 'cjs',
-        sourcemap: true,
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: 'dist/index.js',
+      format: 'cjs',
+      sourcemap: true,
+    },
+    {
+      file: 'dist/index.esm.js',
+      format: 'esm',
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    peerDepsExternal(),
+    resolve(),
+    commonjs(),
+    typescript({ 
+      tsconfig: './tsconfig.json',
+      declaration: true,
+      declarationDir: './dist',
+      rootDir: './src'
+    }),
+    postcss({
+      config: {
+        path: './postcss.config.js',
       },
-      {
-        file: packageJson.module,
-        format: 'esm',
-        sourcemap: true,
+      extensions: ['.css'],
+      minimize: true,
+      inject: {
+        insertAt: 'top',
       },
-    ],
-    plugins: [
-      peerDepsExternal(),
-      resolve(),
-      commonjs(),
-      typescript({ 
-        tsconfig: './tsconfig.json',
-        declaration: true,
-        declarationDir: './dist',
-        rootDir: './src'
-      }),
-      postcss({
-        config: {
-          path: './postcss.config.js',
-        },
-        extensions: ['.css'],
-        minimize: true,
-        inject: {
-          insertAt: 'top',
-        },
-      }),
-      terser(),
-    ],
-    external: ['react', 'react-dom'],
-  },
-  {
-    input: 'dist/index.d.ts',
-    output: [{ file: 'dist/index.d.ts', format: 'es' }],
-    plugins: [dts()],
-    external: [/\.css$/],
-  },
-];
-
-export default config;
+    }),
+    terser(),
+  ],
+  external: ['react', 'react-dom'],
+};
